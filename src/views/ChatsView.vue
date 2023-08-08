f1afd7f753a451ac4a88d200255b66372de72d6f
Leo Franca <leoarkiteto@gmail.com>
2023-08-08 15:08:11 +0100

    :construction: (Components): Create Chat component

---
+++ src/views/ChatsView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  import MessageRow from "@/components/MessageRow.vue";
+</script>
+
+<template>
+  <div
+    id="Messages"
+    class="fixed h-[calc(100vh-100px)] w-[420px] overflow-auto pt-1">
+    <MessageRow />
+  </div>
+</template>
